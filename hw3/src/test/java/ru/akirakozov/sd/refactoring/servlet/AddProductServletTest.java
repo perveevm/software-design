25a22d12b38a77453a7750fcbb402fd772814db7
perveevm <perveev_m@mail.ru>
2022-01-06 21:05:14 +0300

    AddProductServletTest added

---
+++ hw3/src/test/java/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
@@ -0,0 +1,84 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * @author Perveev Mike (perveev_m@mail.ru)
+ */
+public class AddProductServletTest {
+    private static final int PORT = 8081;
+    private static final String REQUEST_FORMAT = "http://localhost:" + PORT + "/add-product?name=%s&price=%s";
+    private Server server;
+
+    @BeforeEach
+    public void prepare() throws Exception {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
+
+        server = new Server(PORT);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
+        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
+        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
+
+        server.start();
+    }
+
+    @AfterEach
+    public void clear() throws Exception {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "DROP TABLE PRODUCT";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
+
+        server.stop();
+    }
+
+    @Test
+    public void addProductTest() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format(REQUEST_FORMAT, "product_name", 1000))).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(response.body(), "OK\n");
+    }
+
+    @Test
+    public void addProductWithNegativePriceTest() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format(REQUEST_FORMAT, "testname", -1))).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(response.body(), "OK\n");
+    }
+}
